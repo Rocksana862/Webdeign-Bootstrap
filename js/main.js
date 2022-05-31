0bb8c7c0d3bd398a3ff5b9a159a1fefa6500c3e5
Rocksana862 <79040920+Rocksana862@users.noreply.github.com>
2022-06-01 02:46:53 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$('.banner_slide').slick({
+    dots: true,
+    arrows:false,
+    autoplay:false,
+    speed: 900,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  //testimonial Sklider
+  $('.slider').slick({
+    infinite: true,
+    slideToShow: 1,
+    slideToScroll:1,
+  }
+
+  );
